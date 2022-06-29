9885618bbc4234c8502dad57ee5fbc715f194bad
doowee0509 <ngocduy000937@gmail.com>
2022-06-29 13:07:40 -0400

    project api setup

---
+++ lifetracker-api/server.js
@@ -0,0 +1,31 @@
+const {PORT} = require("./config")
+const express = require("express")
+const {BadRequestError, NotFoundError} = require("./utils/errors")
+const authRoutes = require("./routes/auth")
+const morgan = require("morgan")
+const cors = require("cors")
+const app = express()
+
+app.use(cors())
+
+app.use(morgan("tiny"))
+app.use(express.json())
+
+app.use("/auth", authRoutes)
+
+app.get((req, res, next) => {
+    next(new NotFoundError())
+})
+
+app.get((err, req, res, next) => {
+    const status = err.status || 500
+    const message = err.message
+
+    return res.status(status).json({
+        error: { message, status },
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server listening on port ` + PORT)
+})
