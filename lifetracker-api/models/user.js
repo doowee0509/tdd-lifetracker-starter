f319dcd31e6e043d2eac270c6afadb5942f15013
doowee0509 <ngocduy000937@gmail.com>
2022-06-29 14:46:56 -0400

    back end

---
+++ lifetracker-api/models/user.js
@@ -0,0 +1,101 @@
+const db = require("../db")
+const bcrypt = require("bcrypt")
+const {BCRYPT_WORK_FACTOR} = require("../config")
+const { BadRequestError, UnauthorizedError } = require("../utils/errors")
+class User {
+    static async makePublicUser(user) {
+        return {
+            id: user.id,
+            email: user.email,
+            username: user.username,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            created_at: user.created_at,
+            updated_at: user.updated_at
+        }
+    }
+
+    static async login(creds) {
+        //required fields are email and password, throw error if either are missing
+        const requiredFields = ["email", 'password']
+        requiredFields.forEach(field => {
+            if (!creds.hasOwnProperty(field)) {
+                throw new BadRequestError(`Missing ${field} in request body.`)
+            }
+        })
+        //looking the user in the db by email
+        const user = await User.fetchUserByEmail(creds.email)
+        if (user) {
+            const isValid = await bcrypt.compare(creds.password, user.password)
+            if (isValid) {
+                return User.makePublicUser(user)
+            }
+        }
+        // if the user is found, compare the password with the submitted password
+        // if they match, return user
+        // else throw an error
+        throw new UnauthorizedError("Invalid email/password")
+    }
+
+    static async register(creds) {
+        //user should submit email, pw, resvp status and # of guest
+        // if any of the field are missing, throw an error
+        const requiredFields = ["email", 'password', 'username', 'first_name', 'last_name']
+        requiredFields.forEach(field => {
+            if (!creds.hasOwnProperty(field)) {
+                throw new BadRequestError(`Missing ${field} in request body.`)
+            }
+        })
+
+        if(creds.email.indexOf('@') <= 0) {
+            throw new BadRequestError('Invalid email')
+        }
+        // make sure no user in db exist with that email
+        // if user exist, throw an error
+        const existingUser = await User.fetchUserByEmail(creds.email)
+        if (existingUser) {
+            throw new BadRequestError(`Duplicate email: ${creds.email}`)
+        }
+        if (existingUser.username === creds.username) {
+            throw new BadRequestError(`Duplicate username: ${creds.username}`)
+        }
+
+        // hash user password
+        
+        const hashedPassword = await bcrypt.hash(creds.password, BCRYPT_WORK_FACTOR)
+        // lowercase email
+        const lowercasedEmail = creds.email.toLowerCase()
+        //create new user in the db with given info
+        const result = await db.query(`
+            INSERT INTO users (
+                email,
+                password,
+                first_name,
+                last_name,
+                username
+            )
+            VALUES ($1, $2, $3, $4, $5)
+            RETURNING id, email, password, first_name, last_name, username, created_at;
+        `,
+        [lowercasedEmail, hashedPassword, creds.firstName, creds.lastName, creds.username]
+        )
+        //return user
+        return User.makePublicUser(result.rows[0])
+    }
+
+    static async fetchUserByEmail(email) {
+        if (!email) {
+            throw new BadRequestError("No email provided")
+        }
+
+        const query = `SELECT * FROM users WHERE email = $1`
+
+        const result = await db.query(query, [email.toLowerCase()])
+
+        const user = result.rows[0]
+
+        return user
+    }
+}
+
+module.exports = User
---
+++ lifetracker-api/routes/auth.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const User = require("../models/user")
+const router = express.Router()
+
+router.get("/me", async function (req, res, next) {
+    try {
+        //get required information and create new user in the database
+
+        const user = await User.update(req.body)
+        console.log(user)
+        return res.status(201).json({ user })
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.post("/login", async function (req, res, next) {
+    try {
+        //get user email and password and try to authenticate
+
+        const user = await User.login(req.body)
+        return res.status(200).json({ user })
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.post("/register", async function (req, res, next) {
+    try {
+        //get required information and create new user in the database
+
+        const user = await User.register(req.body)
+        return res.status(201).json({ user })
+    } catch (err) {
+        next(err)
+    }
+})
+
+
+module.exports = router
